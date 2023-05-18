f0fbdb227de4553b639050d0fcc0beb56a3df5de
YBIO <48740660+YBIO@users.noreply.github.com>
2023-05-18 09:58:20 +0800

    fix bug

--- utils/loss.py
+++ utils/loss.py
@@ -137,15 +137,15 @@ class ContrastiveLoss(nn.Module):
     def __init__(self, batch_size, device='cuda', temperature=0.5):
         super().__init__()
         self.batch_size = batch_size
-        self.register_buffer("temperature", torch.tensor(temperature).to(device))			# 超参数 温度
-        self.register_buffer("negatives_mask", (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool).to(device)).float())		# 主对角线为0，其余位置全为1的mask矩阵
+        self.register_buffer("temperature", torch.tensor(temperature).to(device))	
+        self.register_buffer("negatives_mask", (~torch.eye(batch_size * 2, batch_size * 2, dtype=bool).to(device)).float())	
         
-    def forward(self, emb_i, emb_j):		# emb_i, emb_j 是来自同一图像的两种不同的预处理方法得到
+    def forward(self, emb_i, emb_j):		
         z_i = F.normalize(emb_i, dim=1)     # (bs, dim)  --->  (bs, dim)
         z_j = F.normalize(emb_j, dim=1)     # (bs, dim)  --->  (bs, dim)
 
-        representations = torch.cat([z_i, z_j], dim=0)          # repre: (2*bs, dim)
-        similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)      # simi_mat: (2*bs, 2*bs)
+        representations = torch.cat([z_i, z_j], dim=0)         
+        similarity_matrix = F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)    
         
         sim_ij = torch.diag(similarity_matrix, self.batch_size)         # bs
         sim_ji = torch.diag(similarity_matrix, -self.batch_size)        # bs
